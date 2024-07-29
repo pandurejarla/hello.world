eea63e9b9b751e7e65745f145b0dd5f5c6f25296
pandu rejarla <chandramukesh2022221059@gcrjy.ac.in>
2024-07-29 19:27:19 +0530

    feat: add Simple.java

---
+++ Simple.java
@@ -0,0 +1,8 @@
+public class Simple {
+    public static void main(String[] args) {
+        int num1 = 235564;
+        int num2 = 3566777;
+        int sum = num1 + num2;
+        System.out.println("sum of given two numbers is: " + sum);
+    }
+}
