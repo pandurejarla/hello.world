4448944b56cbe6a775284729b3a7a5de69e4200e
pandu rejarla <chandramukesh2022221059@gcrjy.ac.in>
2024-08-02 17:47:07 +0530

    feat:update customer.java with using methods

--- Customer.java
+++ Customer.java
@@ -7,4 +7,44 @@ public class Customer {
     String name;
     String emailId;
     int salary;
+    byte age;
+
+    // method types
+    /*
+     * 1. no return type and no parameters
+     */
+    void displayCustomerDetails() {
+        System.err.println("customer name is: " + name);
+        System.out.println("customer ID is: " + customerId);
+        System.out.println("customer salary is: " + salary);
+        System.out.println("customer emain is: " + emailId);
+        System.out.println("custromer age is: " + age);
+    }
+
+    // no return type and with parameter
+    void customerMembership(String memberShip) {
+        System.out.println(name + " membership in this company is:" + memberShip);
+    }
+
+    // with return type and no parameters
+    byte discountByAge() {
+        byte discount = 2;
+        if (age >= 50)
+            discount = 40;
+        else if (age >= 20 && age < 50)
+            discount = 20;
+        else
+            discount = 9;
+        return discount;
+    }
+
+    // with return type and with parameters
+    // based on discount percentage
+    double finalPrice(double prize) {
+        byte discountPercentage = discountByAge();
+        double finalPrize = prize - (prize * discountPercentage) / 100;
+        return finalPrize;
+
+    }
+
 }
