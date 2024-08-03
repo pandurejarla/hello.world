203acb0019378578dc59c9d68e8144c95151ede9
pandu rejarla <chandramukesh2022221059@gcrjy.ac.in>
2024-08-03 10:37:42 +0530

    feat:add one program

---
+++ com/training/javacourse/Multiplication.java
@@ -0,0 +1,11 @@
+package com.training.javacourse;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        byte num1=23;
+        byte num2=10;
+        int result=num2*num1;
+        System.out.println("multiplication of given two values is:"+result);
+    }
+}
+
