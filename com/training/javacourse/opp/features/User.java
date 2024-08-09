d5ec3d4422d70bbfefec24c2f626e1863d03d857
pandu rejarla <chandramukesh2022221059@gcrjy.ac.in>
2024-08-09 14:08:06 +0530

    feat:modified all files fully abstrcition

--- com/training/javacourse/opp/features/User.java
+++ com/training/javacourse/opp/features/User.java
@@ -2,13 +2,13 @@ package com.training.javacourse.opp.features;
 
 public class User {
     public static void main(String[] args) {
-        Lenovo lenovo=new Lenovo();
+        Laptop lenovo=new Lenovo();
         lenovo.copy();
         lenovo.paste();
         lenovo.cut();
         lenovo.keyboard();
         System.out.println("");
-        Hp hp=new Hp();
+        Laptop hp=new Hp();
         hp.copy();
         hp.paste();
         hp.cut();
